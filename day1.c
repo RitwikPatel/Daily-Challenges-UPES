39ee908acd2cec77390195c52a24578c38ee05df
RitwikPatel <patelritwik2008@gmail.com>
2026-08-16 19:13:38 +0530

    Day 1 - sum of two numbers

---
+++ day1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    scanf("%d %d", &num1, &num2);
+
+    sum = num1 + num2;
+
+    printf("Sum = %d\n", sum);
+
+return 0;
+}
+
+//sample input
+// 3 4 = 7
+
+//-1 20 = 19
