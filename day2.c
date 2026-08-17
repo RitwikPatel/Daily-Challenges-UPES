1ef41b5f131b65ff4581f754ca9088e45487c3dd
Ritwik <77725805+RitwikPatel@users.noreply.github.com>
2026-08-17 14:40:22 +0530

    Add files via upload

---
+++ day2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    int sum, diff, product, quotient;
+
+    scanf("%d %d", &num1, &num2);
+
+    sum = num1 + num2;
+    diff = num1 - num2;
+    product = num1 * num2;
+    quotient = num1 / num2;
+
+        printf("Sum=%d, Diff=%d, Product=%d, Quotient=%d\n",
+               sum, diff, product, quotient);
+
+    return 0;
+}
